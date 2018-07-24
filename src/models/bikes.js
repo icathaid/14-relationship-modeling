0930375ee67ff5f7f18bdf94c11a26f14af01703
James McDaniel <icathaid@gmail.com>
2018-07-23 21:05:45 -0700

    added model

--- src/models/bikes.js
+++ src/models/bikes.js
@@ -1,1 +1,10 @@
-//  copy model from lab 13
+'use strict';
+import mongoose from 'mongoose';
+
+const bikeSchema = mongoose.Schema({
+  make: {type:String, required:true},
+  // model: {type:String, required:true},
+  // speed: {type:Number},
+});
+
+export default mongoose.model('bikes', bikeSchema);
